3262f9ddd31ac5de4a3aba3bbac68f2b6ef16bfe
donrosszx <donrosszx@gmail.com>
2016-02-25 16:43:45 -0600

    added /todos and todos/:id routes

--- server.js
+++ server.js
@@ -1,9 +1,43 @@
 var express = require('express');
 var app = express();
 var PORT = process.env.port || 3000;
+var todos = [{
+    id: 1,
+    description: 'Meet mom for lunch',
+    completed: false
+}, {
+    id: 2,
+    description: 'Go to market',
+    completed: false
+}, {
+    id: 3,
+    description: 'Get kids off bus',
+    completed: true
+}];
 
 app.get('/', function (req, res) {
     res.send('Todo API Root');
+});
+
+app.get('/todos', function (req, res) {
+    res.json(todos);
+});
+
+app.get('/todos/:id', function (req, res) {
+    var todoId = parseInt(req.params.id, 10);
+    var match;
+    // iterate over todos area.  find the match
+    todos.forEach(function (todo) {
+        if (todo.id === todoId) {
+            match = todo;
+        }
+    });
+    // res.send(typeof match);
+    if (match) {
+        res.json(match);
+    } else {
+        res.status(404).send();
+    }
 });
 
 app.listen(PORT, function () {
