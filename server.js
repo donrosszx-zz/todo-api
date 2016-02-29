9cb8e6ca515c590d2cc47f975d6ce888dbd0eda5
donrosszx <donrosszx@gmail.com>
2016-02-29 09:53:00 -0600

    add user model and POST users route

--- server.js
+++ server.js
@@ -121,8 +121,17 @@ app.put('/todos/:id', function(req, res) {
         res.status(400).json(e);
     });
 
-    // _.extend(match, attributes);
-    // res.json(match);
+});
+
+// POST /users
+app.post('/users', function(req, res) {
+    var body = _.pick(req.body, 'email', 'password');
+
+    db.user.create(body).then(function (user) {
+        res.json(user.toJSON());
+    }, function (e) {
+        res.status(400).json(e);
+    });
 
 });
 
