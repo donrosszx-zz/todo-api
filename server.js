950294a26d3c2866d2eebe0fb59579eed07a9f17
donrosszx <donrosszx@gmail.com>
2016-02-29 12:16:54 -0600

    refactored login to a class method

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
+var bcrypt = require('bcrypt');
 
 var db = require('./db.js');
 
@@ -135,7 +136,18 @@ app.post('/users', function(req, res) {
 
 });
 
-db.sequelize.sync().then(function() {
+// POST /users/login
+app.post('/users/login', function (req, res) {
+    var body = _.pick(req.body, 'email', 'password');
+
+    db.user.authenticate(body).then(function (user) {
+        res.json(user.toPublicJSON());
+    }, function () {
+        res.status(401).send();
+    });
+});
+
+db.sequelize.sync({force: true}).then(function() {
     app.listen(PORT, function() {
         console.log("Express listening on PORT " + PORT);
     });
