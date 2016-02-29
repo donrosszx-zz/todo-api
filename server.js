cf83d20912bd1d74e5f87bd79caaaa73728f6c42
donrosszx <donrosszx@gmail.com>
2016-02-28 21:10:25 -0600

    updated the PUT/update route to use a db

--- server.js
+++ server.js
@@ -95,31 +95,34 @@ app.delete('/todos/:id', function(req, res) {
 // PUT /todos/:id
 app.put('/todos/:id', function(req, res) {
     var todoId = parseInt(req.params.id, 10);
-    var match = _.findWhere(todos, {
-        id: todoId
-    });
-    if (!match) {
-        return res.status(404).send();
-    }
-
     var body = _.pick(req.body, 'description', 'completed');
-    var validAttributes = {};
+    var attributes = {};
 
     // body.hasOwnProperty('completed')
-    if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
-        validAttributes.completed = body.completed;
-    } else if (body.hasOwnProperty('completed')) {
-        return res.status(400).send();
+    if (body.hasOwnProperty('completed')) {
+        attributes.completed = body.completed;
     }
 
-    if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
-        validAttributes.description = body.description;
-    } else if (body.hasOwnProperty('description')) {
-        return res.status(400).send();
+    if (body.hasOwnProperty('description')) {
+        attributes.description = body.description;
     }
 
-    _.extend(match, validAttributes);
-    res.json(match);
+    db.todo.findById(todoId).then(function (todo) {
+        if (todo) {
+            return todo.update(attributes);
+        } else {
+            res.status(404).send();
+        }
+    }, function () {
+        res.status(500).send();
+    }).then(function (todo) {
+        res.json(todo.toJSON());
+    }, function (e) {
+        res.status(400).json(e);
+    });
+
+    // _.extend(match, attributes);
+    // res.json(match);
 
 });
 
