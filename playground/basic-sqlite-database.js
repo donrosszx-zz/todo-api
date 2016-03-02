5c3b902d00c65ed31e6134f8ae9a9375557d14ec
donrosszx <donrosszx@gmail.com>
2016-03-02 09:09:19 -0600

    added todo/user association

--- playground/basic-sqlite-database.js
+++ playground/basic-sqlite-database.js
@@ -19,46 +19,39 @@ var Todo = sequelize.define('todo', {
     }
 });
 
+var User = sequelize.define('user', {
+    email: Sequelize.STRING //shortcut if only type is defined
+});
+
+Todo.belongsTo(User);
+User.hasMany(Todo);
+
 sequelize.sync({
     // force: true
 }).then(function() {
     console.log('Everything is synced');
 
-    Todo.findById(3).then(function (todo) {
-        if (todo) {
-            console.log(todo.toJSON());
-        } else {
-            console.log('todo not found');
-        }
+    User.findById(1).then(function (user) {
+        user.getTodos({
+            where: {
+                completed: false
+            }
+        }).then(function (todos) {
+            todos.forEach(function (todo) {
+                console.log(todo.toJSON());
+            });
+        });
     });
 
-    // Todo.create({
-    //     description: "Take out trash"
-    // }).then(function(todo) {
+    // User.create({
+    //     email: 'donr@vmix.com'
+    // }).then(function(user) {
     //     return Todo.create({
-    //         description: 'Clean office'
+    //         description: 'Shave the llama'
     //     });
-    // }).then(function () {
-    //     // return Todo.findById(1);
-    //     return Todo.findAll({
-    //         where: {
-    //             // completed: false
-    //             description: {
-    //                 $like: '%trash%'
-    //             }
-    //         }
+    // }).then(function(todo) {
+    //     User.findById(1).then(function(user) {
+    //         user.addTodo(todo);
     //     });
-    // }).then(function (todos) {
-    //     if (todos) {
-    //         todos.forEach(function (todo) {
-    //             console.log(todo.toJSON());
-    //         });
-    //         console.log(todos.toJSON());
-    //     } else {
-    //         console.log('no todos found');
-    //     }
-    // })
-    // .catch(function (e) {
-    //     console.log(e);
     // });
 });
